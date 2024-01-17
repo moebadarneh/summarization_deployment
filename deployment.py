1fe5df7e14001389b34e1b4b6b66e2babd676692
moebadarneh <mohammad.badarneh@student.htw-berlin.de>
2024-01-17 17:55:06 +0100

    update

--- deployment.py
+++ deployment.py
@@ -1,29 +1,14 @@
 
-from flask import Flask, request, jsonify
-from transformers import pipeline
-
+from flask import Flask, jsonify
+ 
 app = Flask(__name__)
 
-# Load the summarization model from Hugging Face
-summarizer = pipeline("summarization")
-
-@app.route('/summarize', methods=['GET'])
-def summarize_text():
-    try:
-        # Get the input text from the request
-        data = request.get_json()
-        input_text = data['text']
-
-        # Perform summarization
-        summary = summarizer(input_text, max_length=50, min_length=15,do_sample=False)
-
-        # Extract the summarized text from the result
-        summarized_text = summary[0]['summary_text'].strip()
-
-        # Return the summarized text as JSON
-        return jsonify({'summary': summarized_text})
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+@app.route('/')
+def get_data():
+    data = {
+        'name': 'Dhanush',
+        'age': 20,
+        'city': 'Hyderabad'
+    }
+    return jsonify(data)
 
